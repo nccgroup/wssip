7488b1a573bf8248a9ab9e78b10ceb5905ba7028
Samantha Chalker <samantha.chalker@nccgroup.trust>
2017-05-10 17:47:01 -0400

    Expiration for certificates is 2 years based upon Modified Time instead of Created Time. Fix main.js for returning the right version on 'wssip --version'. Add in WIP Homebrew formula.

---
+++ wssip.rb
@@ -0,0 +1,24 @@
+require "language/node"
+
+class Wssip < Formula
+  desc "Proxy application for manipulating WebSocket messages"
+  homepage "https://github.com/nccgroup/wssip"
+  url "https://registry.npmjs.org/wssip/-/wssip-1.0.5.tgz"
+  version "1.0.5"
+  sha256 ""
+  head "https://github.com/nccgroup/wssip.git"
+
+  bottle :unneeded
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", "--production", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink "#{libexec}/bin/wssip" => "wssip"
+  end
+
+  test do
+    vers = system bin/"wssip", "--version"
+    assert_equal vers, '1.0.5'
+  end
+end
