88609df7f92846e052ab2e218e6bd09c7c57c29a
Samantha Chalker <samantha.chalker@nccgroup.trust>
2017-05-11 17:33:52 -0400

    Move dependencies to either dev or optional

--- build/wssip.rb
+++ build/wssip.rb
@@ -3,9 +3,9 @@ require "language/node"
 class Wssip < Formula
   desc "Proxy application for manipulating WebSocket messages"
   homepage "https://github.com/nccgroup/wssip"
-  url "https://registry.npmjs.org/wssip/-/wssip-1.0.6.tgz"
-  version "1.0.6"
-  sha256 "84e3589e2ba8f3e4dfff49421feba770bc21e723526c145300f753b9979a81bd"
+  url "https://registry.npmjs.org/wssip/-/wssip-1.0.7.tgz"
+  version "1.0.7"
+  #sha256 ""
   head "https://github.com/nccgroup/wssip.git"
 
   bottle :unneeded
@@ -19,6 +19,6 @@ class Wssip < Formula
 
   test do
     vers = system bin/"wssip", "--version"
-    assert_equal vers, '1.0.6'
+    assert_equal vers, '1.0.7'
   end
 end
